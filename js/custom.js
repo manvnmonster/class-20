0af0faf6c7e897baed135219679d0b26e17d1706
manvnmonster <119920859+manvnmonster@users.noreply.github.com>
2023-01-09 22:57:37 -0800

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,35 @@
+/*var a = 'Akash',
+    b = 'Batash',
+    h = 'Habiba',
+    A = 'Afroza',
+    proOffer = '50% Discount!';
+
+alert(a);*/
+
+// document.write(proOffer);
+
+
+
+function box() {
+	alert("We Are Learning JS");
+}
+
+var offer = document.getElementById("phone");
+
+offer.style.color = "#badc58";
+
+offer.innerHTML = "uses and abuses mobile phone";
+
+ var apple = document.getElementById("button");
+ button.style.color = "#badc58";
+
+
+var abc = "hlw",
+	xyz = "whats",
+	pqr = "up",
+	mno = "guys";
+
+alert(abc);
+alert(xyz);
+alert(pqr);
+alert(mno);
